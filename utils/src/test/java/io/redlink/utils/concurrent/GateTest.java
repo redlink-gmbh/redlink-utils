3040967c0486698fdfd3635229ce7246e38400e8
Jakob Frank <jakob.frank@redlink.at>
2021-07-23 11:30:42 +0200

    Fix deprecation warnings

--- utils/src/test/java/io/redlink/utils/concurrent/GateTest.java
+++ utils/src/test/java/io/redlink/utils/concurrent/GateTest.java
@@ -27,8 +27,8 @@ import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
